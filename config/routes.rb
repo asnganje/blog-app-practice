a4eeb4984d95ae3cf1847df18137737a28891449
asnganje <abd.nganje@gmail.com>
2025-03-28 06:01:06 +0300

    add CRUD operations for article

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root "articles#index"
-  post "/articles", to: "articles#create"
-  get "/articles/new", to: "articles#new"
-  get "/articles/:id", to: "articles#show", as: :article
+  resources :articles
+  # delete "/articles/:id", to: "articles#destroy"
+  # patch "articles/:id", to: "articles#update"
+  # post "/articles", to: "articles#create"
+  # get "/articles/new", to: "articles#new"
+  # get "/articles/:id/edit", to: "articles#edit", as: :edit_article
+  # get "/articles/:id", to: "articles#show", as: :article
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
